645a2134d80b8cdb94c7e0046b5ce914c107fe0c
j242h828 <jacob.horton@ku.edu>
2022-02-16 17:49:59 -0600

    Upload New File

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "executive.h"
+
+int main(){
+    Executive exec; // creates an instance of the Executive class
+    exec.run(); // calls the run function within the Executive class
+    return 0;
+}
