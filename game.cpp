3c022cbc6c2e70e716314a5ee9c6629bbe9bb562
Julia Nichols <julia.nichols@ku.edu>
2022-02-16 18:13:44 -0600

    S fix

--- game.cpp
+++ game.cpp
@@ -513,7 +513,7 @@ void Game::playerGuess() // game class 'playerGuess' function that asks for play
 
         if (!gameFinished) // if the game isn't finished...
         {
-            Sleep(3); //wait three seconds
+            sleep(3); //wait three seconds
             for (int i = 0; i < 100; i++) // loop 100 times
             {
                 std::cout << std::endl; // new line (effectively clearing the command prompt)
@@ -574,7 +574,7 @@ void Game::playerGuess() // game class 'playerGuess' function that asks for play
             }
             // printBoardP2();
         }
-        Sleep(3); // wait 3 seconds
+        sleep(3); // wait 3 seconds
     }
 }
 
